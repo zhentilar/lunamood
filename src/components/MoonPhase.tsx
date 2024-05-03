e3ff3f0d30292296da4299e02c2c100201c74c78
Zhentarim <54887323+zhentilar@users.noreply.github.com>
2024-05-03 17:59:00 +0300

    ok

--- src/components/MoonPhase.tsx
+++ src/components/MoonPhase.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import ProgressBar from './ProgressBar';
+import ProgressBar from './ProgressBar.tsx';
 
 const PhaseContainer = styled.div`
   display: flex;
