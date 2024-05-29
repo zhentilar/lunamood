c21057f0c992d59a36810b3e536dac0bac89d65e
Zhentarim <54887323+zhentilar@users.noreply.github.com>
2024-05-29 18:44:11 +0300

    h5p update

--- src/components/H5PComponent.tsx
+++ src/components/H5PComponent.tsx
@@ -4,15 +4,12 @@ const H5PComponent: React.FC = () => {
   return (
     <>
       <div style={{ marginBottom: '40px', marginTop: '40px' }}>
-        <iframe
-          src="https://h5p.org/h5p/embed/1489031"
-          width={1091}
-          height={500}
-          frameBorder={0}
-          allowFullScreen={true}
-          allow="geolocation *; microphone *; camera *; midi *; encrypted-media *"
-          title="LunaMood Sorular"
-        ></iframe>
+        <iframe src="https://h5p.org/h5p/embed/1489834" 
+        width="1091" height="380" 
+        frameBorder="0" 
+        allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" 
+        title="LunaMood Quiz">
+        </iframe>
       </div>
       <script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charSet="UTF-8"></script>
     </>
